4c3f83c4468fe37849e7a279bf933bba84c4d994
Indraneel Ghosh <indraneel.ghosh1998@gmail.com>
2020-02-16 23:12:18 +0530

    Lexer Code (#3)

--- lexer.c
+++ lexer.c
@@ -1,210 +1,594 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include "lexerDef.h"
+#include<stdio.h>
 #include "lexer.h"
-//lines are 1 indexed
-static ull linenum = 1;
-char x[BUFFER_SIZE];
-//block must be modularized
-node* create_node()
-{
-	node* temp = (node*)(malloc(sizeof(node)));
-	return temp;
-}
-header set_header()
-{
+#include<stdlib.h>
+#include"lextable.h"
+#include<string.h>
+#include<stdbool.h>
+#include<ctype.h>
+#define BUF 4096
+static char buffer0[BUF+1];
+static char buffer1[BUF+1];
+static int lexemeBegin, forward;
+static bool lexflag=0, forflag=0;
+static FILE* fptr;
+int line;
+int check = 1;
 
-}
-void flush(char* str)
-{
+lextable table;
 
+void removeComments(char* testfile, char* cleanfile)
+{
+    return;
 }
 
-//check if it is a keyword
-char* keyword_check(char* inp)
+int getLineNumber()
 {
-	for(int i=0;i<MAX_SIZE;i++)
+    return line;
+}
+
+
+FILE* getStream(FILE* fptr)
+{
+    if(forward==BUF) //end of buffer
+    {
+	if(forflag == 0)
 	{
-		if(strcmp(inp,keyword_dict[i])==0)// it's a keyword
+	    int count = fread(buffer1,1,BUF,fptr);
+	    buffer1[count]=EOF;
+	    forflag = 1;
+	    forward = 0;
+	}
+	else
+	{
+	    int count = fread(buffer0,1,BUF,fptr);
+	    buffer0[count]=EOF;
+	    forflag = 0;
+	    forward = 0;
+	}
+	return fptr;
+    }
+    else
+    {
+	fclose(fptr);
+	return NULL; //terminate lexical analysis
+    }	
+}
+
+
+token* id()
+{
+    char ans[100];
+    int len= 0;
+    char peek;
+
+    while(1)
+    {
+		if(!forflag)
+			peek = buffer0[forward];
+		else
+			peek = buffer1[forward];
+		
+		if(peek == EOF)
+			fptr = getStream(fptr);
+		else
 		{
-			char *s = (char*)(malloc(sizeof(char)*(MAX_SIZE+1)));
-			strcpy(s,token_dict[i]);
-			return s;
+			if(isalpha(peek) || isdigit(peek) || peek == '_')
+				ans[len++] = peek, forward++;
+			else
+			{
+				ans[len]=0;
+				lexemeBegin = forward;
+				return insertTable(table,ans,ID);
+			}
 		}
-	}
-	char temp[] = "NO";
-	return temp;	
+    }
 }
 
-// Removes newline '\n' '\r' present in lang spec?
-int remove_lines(char* in,int x)
-{
-	while(in[x]=='\r' && in[++x]=='\n'){
-		linenum++;
-		x++;
-	}
-	return x;
-}
 
-// Removes comments
-int remove_comments(char* in,int x)
+token* number()
 {
-	while(!(in[x]=='*' && in[x+1]=='*')){
-		x++;
-		if(in[x] == '\n'){
-			linunum++;	
-		}
-	}
-	return x;
-}
-FILE* reloadBuffer(char* file)// input text from test file into buffer 
-{
-
-}
-void remove(char* file)
-{
-
-}
-
-node* getToken()
-{
-//getch
-	char* inp = (char*)malloc(sizeof(char)*(MAX_SIZE+1));
-	char* token = (char*)malloc(sizeof(char)*(MAX_SIZE+1));
-	//flush(inp);
-	//flush(token);
-	char c;
+    char ans[100];
+    char peek;
 	int len = 0;
-	//need to use peek
-	while(true){
-		c = x[end];
-		switch(state){
-			case 0:	
-				if(c=='+'){
-					node* lexer = createnode();
-					strcpy(lexer->token,"PLUS");
-					strcpy(lexer->value,'+');
-					lexer->lineNo = linenum;
-					end++;
-					begin=end;
-					state=0;
-					numTokens++;
-					return lexer;
-					break;
-				} else if(c=='-'){
-					node* lexer = createnode();
-					strcpy(lexer->token,"MINUS");
-					strcpy(lexer->value,'-');
-					lexer->lineNo = linenum;
-					end++;
-					begin=end;
-					state=0;
-					numTokens++;
-					return lexer;
-					break;
-				} else if(c=='/'){
-					node* lexer = createnode();
-					strcpy(lexer->token,"DIV");
-					strcpy(lexer->value,'/');
-					lexer->lineNo = linenum;
-					end++;
-					begin=end;
-					state=0;
-					numTokens++;
-					return lexer;
-					break;
-				} else if(c==';'){
-					node* lexer = createnode();
-					strcpy(lexer->token,"SEMCOL");
-					strcpy(lexer->value,';');
-					lexer->lineNo = linenum;
-					end++;
-					begin=end;
-					state=0;
-					numTokens++;
-					len=0;
-					return lexer;
-					break;
-				} else if(c=='['){
-					node* lexer = createnode();
-					strcpy(lexer->token,"SQBO");
-					strcpy(lexer->value,'[');
-					lexer->lineNo = linenum;
-					end++;
-					begin=end;
-					state=0;
-					numTokens++;
-					len=0;
-					return lexer;
-					break;
-				} else if(c==']'){
-					node* lexer = createnode();
-					strcpy(lexer->token,"SQBC");
-					strcpy(lexer->value,';');
-					lexer->lineNo = linenum;
-					end++;
-					begin=end;
-					state=0;
-					numTokens++;
-					len=0;
-					return lexer;
-					break;
-				} else if(c=='('){
-					node* lexer = createnode();
-					strcpy(lexer->token,"BO");
-					strcpy(lexer->value,';');
-					lexer->lineNo = linenum;
-					end++;
-					begin=end;
-					state=0;
-					numTokens++;
-					len=0;
-					return lexer;
-					break;
-				} else if(c==')'){
-					node* lexer = createnode();
-					strcpy(lexer->token,"BC");
-					strcpy(lexer->value,';');
-					lexer->lineNo = linenum;
-					end++;
-					begin=end;
-					state=0;
-					numTokens++;
-					len=0;
-					return lexer;
-					break;
-				} else if(c==','){
-					node* lexer = createnode();
-					strcpy(lexer->token,"COMMA");
-					strcpy(lexer->value,';');
-					lexer->lineNo = linenum;
-					end++;
-					begin=end;
-					state=0;
-					numTokens++;
-					len=0;
-					return lexer;
-					break;
-				} else if(c=='$'){
-					node* lexer = createnode();
-					strcpy(lexer->token,"END");
-					strcpy(lexer->value,'$');//$ is End of string marker?
-					lexer->lineNo = linenum;
-					linenum=1;
-					end=0;
-					begin=0;
-					state=0;
-					return lexer;
-					break;
-				} 
+	int state = 0;
+    while(1)
+    {
+		if(!forflag)
+			peek = buffer0[forward];
+		else
+			peek = buffer1[forward];
+
+		if(peek == EOF)
+			fptr = getStream(fptr);
+		else
+		{
+		
+			//make dfa for number literals
+			switch(state)
+			{
+				case 0: if(isdigit(peek))
+						{
+							ans[len++] = peek; 
+							forward++;
+							state = 1;
+						}
+						else
+						{
+							printf("Wrong call. The function should not be called without a num\n");
+							return NULL;
+						}
+						break;
+				case 1: if(isdigit(peek))
+						{
+							ans[len++] = peek; 
+							forward++;
+							state = 1;
+						}
+						else if(peek == '.')
+						{
+							ans[len++] = peek;
+							forward++;
+							state = 2;
+						}
+						else
+						{
+							
+							ans[len]=0;
+							lexemeBegin = forward;
+							return insertTable(table,ans,NUM);
+						}
+						break;
+				case 2: if(isdigit(peek))
+						{
+							ans[len++] = peek; 
+							forward++;
+							state = 3;
+						}
+						else if(peek == '.')
+						{
+							forward--;
+							ans[--len]=0;
+							lexemeBegin = forward;
+							return insertTable(table,ans,NUM);
+						}
+						else
+						{
+							return NULL;
+						}
+						break;
+				case 3: if(isdigit(peek))
+						{
+							ans[len++] = peek; 
+							forward++;
+							state = 3;
+						}
+						else if(peek == 'e' || peek == 'E')
+						{
+							ans[len++]=peek;
+							forward++;
+							state = 4;
+						}
+						else
+						{
+							ans[len]=0;
+							lexemeBegin = forward;
+							return insertTable(table,ans, RNUM);
+						}
+						break;
+				case 4: if(peek=='+' || peek=='-')
+						{
+							ans[len++] = peek; 
+							forward++;
+							state = 5;
+						}
+						else if(isdigit(peek))
+						{
+							ans[len++]=peek;
+							forward++;
+							state = 6;
+						}
+						else
+						{
+							return NULL;
+						}
+						break;
+				case 5: if(isdigit(peek))
+						{
+							ans[len++] = peek; 
+							forward++;
+							state = 6;
+						}
+						else
+						{
+							return NULL;
+						}
+						break;
+				case 6: if(isdigit(peek))
+						{
+							ans[len++] = peek; 
+							forward++;
+							state = 6;
+						}
+						else
+						{
+							ans[len]=0;
+							lexemeBegin = forward;
+							return insertTable(table,ans, RNUM);
+						}
+						break;
+				default: 
+						printf("unknown state reached");
+						return NULL;
+						break;
+			}
+    }
+}
+
+
+token* operation()
+{
+    char ans[3];
+    char peek;
+	int state = 0;
+	int len = 0;
+    while(1)
+    {
+	if(!forflag)
+	    peek = buffer0[forward];
+	else
+	    peek = buffer1[forward];
+	if(peek == EOF)
+	    fptr = getStream(fptr);
+	else
+	{
+	    switch(state)
+		{
+			case 0: 
+			{
+				switch(peek)
+				{
+					case '*': ans[len++]=peek;
+							  forward++;
+							  state = 1;
+							  break;
+					case '<':ans[len++]=peek;
+							  forward++;
+							  state = 2;
+							  break;
+					case '>':ans[len++]=peek;
+							  forward++;
+							  state = 3;
+							  break;
+					case '!':ans[len++]=peek;
+							  forward++;
+							  state = 4;
+							  break;
+					case '=':ans[len++]=peek;
+							  forward++;
+							  state = 5;
+							  break;
+					case ':':ans[len++]=peek;
+							  forward++;
+							  state = 6;
+							  break;
+					case '.':ans[len++]=peek;
+							  forward++;
+							  state = 7;
+							  break;
+					
+					
+					
+					case '+':ans[len++]=peek;
+							ans[len]=0;
+							forward++;
+							lexemeBegin = forward;							
+							return insertTable(table, ans, Token);
+							break;
+					case '-':ans[len++]=peek;
+							ans[len]=0;
+							forward++;
+							lexemeBegin = forward;							
+							return insertTable(table, ans, Token);
+							break;
+					case '/':ans[len++]=peek;
+							ans[len]=0;
+							forward++;
+							lexemeBegin = forward;							
+							return insertTable(table, ans, Token);
+							break;
+					case ';':ans[len++]=peek;
+							ans[len]=0;
+							forward++;
+							lexemeBegin = forward;							
+							return insertTable(table, ans, Token);
+							break;
+					case ',': ans[len++]=peek;
+							ans[len]=0;
+							forward++;
+							lexemeBegin = forward;							
+							return insertTable(table, ans, Token);
+							break;
+					case ']': ans[len++]=peek;
+							ans[len]=0;
+							forward++;
+							lexemeBegin = forward;							
+							return insertTable(table, ans, Token);
+							break;
+					case '[': ans[len++]=peek;
+							ans[len]=0;
+							forward++;
+							lexemeBegin = forward;							
+							return insertTable(table, ans, Token);
+							break;
+					case ')':ans[len++]=peek;
+							ans[len]=0;
+							forward++;
+							lexemeBegin = forward;							
+							return insertTable(table, ans, Token);
+							break;
+					case '(':ans[len++]=peek;
+							ans[len]=0;
+							forward++;
+							lexemeBegin = forward;							
+							return insertTable(table, ans, Token);
+							break;
+				}
+			}
+			break;
 			case 1:
-				
+				if(peek=='*')
+				{
+					ans[len++]=peek;
+					forward++;
+					state=8;					
+				}
+				else
+				{
+					ans[len] = 0;
+					lexemeBegin = forward;
+					return insertTable(table,ans,MULTIPLY);
+				}
 			case 2:
-				
+				if(peek=='<')
+				{
+					ans[len++]= peek;
+					forward++;
+					ans[len] = 0;
+					lexemeBegin = forward;
+					return insertTable(table, ans, DEF);
+				}
+				else if(peek == '=')
+				{
+					ans[len++]= peek;
+					forward++;
+					ans[len] = 0;
+					lexemeBegin = forward;
+					return insertTable(table, ans, LE);	
+				}
+				else
+				{
+					ans[len] = 0;
+					lexemeBegin = forward;
+					return insertTable(table, ans, LT);
+				}
+				break;
 			case 3:
-				
+				if(peek=='>')
+				{
+					ans[len++]= peek;
+					forward++;
+					ans[len] = 0;
+					lexemeBegin = forward;
+					return insertTable(table, ans, ENDDEF);
+				}
+				else if(peek == '=')
+				{
+					ans[len++]= peek;
+					forward++;
+					ans[len] = 0;
+					lexemeBegin = forward;
+					return insertTable(table, ans, GE);	
+				}
+				else
+				{
+					ans[len] = 0;
+					lexemeBegin = forward;
+					return insertTable(table, ans, GT);
+				}
+				break;
 			case 4:
+				if(peek=='=')
+				{
+					ans[len++] = peek;
+					forward++;
+					ans[len] = 0;
+					lexemeBegin = forward;
+					return insertTable(table, ans, NE);
+				}
+				else
+				{
+					return NULL;
+				}
+				break;
+			case 5:
+				if(peek=='=')
+				{
+					ans[len++] = peek;
+					forward++;
+					ans[len] = 0;
+					lexemeBegin = forward;
+					return insertTable(table, ans, EQ);
+				}
+				else
+				{
+					return NULL;
+				}
+				break;
+			case 6:
+				if(peek=='=')
+				{
+					ans[len++]= peek;
+					forward++;
+					ans[len] = 0;
+					lexemeBegin = forward;
+					return insertTable(table, ans, ASSIGNOP);
+				}
+				else
+				{
+					ans[len] = 0;
+					lexemeBegin = forward;
+					return insertTable(table, ans, COLON);
+				}
+				break;
+			case 7:
+				if(peek=='.')
+				{
+					ans[len++] = peek;
+					forward++;
+					ans[len] = 0;
+					lexemeBegin = forward;
+					return insertTable(table, ans, RANGEOP);
+				}
+				else
+				{
+					return NULL;
+				}
+				break;
+			case 8:
+				if(peek=='*')
+				{
+					forward++;
+					state = 9;
+				}
+				else
+				{
+					forward++;
+				}
+				break;
+			case 9: 
+				if(peek == '*')
+				{
+					forward++;
+					return insertTable(table, ans, COMMENTMARK);
+				}
+				else
+				{
+					forward++;
+					state = 8;
+				}
+				break;
 		}
 	}
+    }
+}
+
+void openfile(char* sourcefile)
+{
+	fptr = fopen(sourcefile, "rb");
+}
+
+token* getNextToken()
+{
+    //initialization code
+	if(!fptr)
+	{
+		printf("file not open\n");
+		exit(1);
+	}	
+    if(check)
+    {
+		check = 0;
+		table = getLexTable(100);
+		int count = fread(buffer0, 1, BUF,fptr);
+		line = 1;
+		buffer0[count]=EOF; //sentinel value to mark the end of buffer
+		lexemeBegin = 0;
+		forward = 0;
+    }
+    while(1)
+    {
+	//start state for the dfa
+	char peek;
+	if(!forflag)
+	    peek = buffer0[forward];
+	else
+	    peek = buffer1[forward];
+
+	switch(peek)
+	{
+	    case EOF:
+		    fptr = getStream(fptr);
+		    break;
+
+	    case ' ': case '\t':  //ignore spaces
+		    forward++;
+		    break;
+
+	    case '\n':
+		    forward++;  //ignore spaces and count line number
+		    line++;
+		    break;
+	    
+	    case 'A': case 'B': case 'C': case 'D': case 'E':
+	    case 'F': case 'G': case 'H': case 'I': case 'J':
+	    case 'K': case 'L': case 'M': case 'N': case 'O':
+	    case 'P': case 'Q': case 'R': case 'S': case 'T':
+	    case 'U': case 'V': case 'W': case 'X': case 'Y':
+	    case 'Z': case 'a': case 'b': case 'c': case 'd':
+	    case 'e': case 'f': case 'g': case 'h': case 'i': 
+	    case 'j': case 'k': case 'l': case 'm': case 'n':
+	    case 'o': case 'p': case 'q': case 'r': case 's':
+	    case 't': case 'u': case 'v': case 'w': case 'x':
+	    case 'y': case 'z':
+		    {
+				token* temp = id();
+				if(strlen(temp->str)>20) //raise error
+				{
+				printf("Token Length error: Line %d\n",line);
+				}
+				else
+					return temp;
+				break;
+			}
+	    
+
+	    case '0': case '1': case '2': case '3': case '4':    // tokenize a INT or REAL.
+	    case '5': case '6': case '7': case '8': case '9':
+		    {	token* temp = number();
+			if(temp)
+			    return temp;
+			else
+			{
+			    printf("Invalid Number Error: Line %d\n", line);
+			    lexemeBegin = forward;
+			}
+			break;
+		    }
+	    
+	    
+	    case '+': case '-': case '*': case '/': case '<':   //Tokenize Other symbols.
+	    case '>': case '=': case '!': case ';': case '.':
+	    case ':': case ',': case '[': case ']': case '(':
+	    case ')': 
+		    {
+			token* temp = operation();
+			if(temp)
+			    return temp;
+			else
+			{
+			    printf("Invalid Operation Error: Line %d\n",line);
+			    lexemeBegin = forward;
+			}
+		    break;}
+
+	    default:
+		{
+		    printf("Unrecognized Symbol Error: Line %d\n", line);
+		    lexemeBegin++;
+		    forward++;
+		    break;
+		}
+	}
+    }
+
 }
