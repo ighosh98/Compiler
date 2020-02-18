73a435b1555a4a79bb336ddadf6e23714a410e90
ayushvachaspati <avachasapti97@gmail.com>
2020-02-18 23:47:38 +0530

    addid str to enum table

--- src/parser.c
+++ src/parser.c
@@ -7,14 +7,9 @@ hashtable strToEnum;
 
 void make_str_to_enum()
 {
-    printf("\n\n");
     strToEnum = getHashTable(100);
-    printf("62: %s\n",symbol_map[62]);
     for(int i=ENUM_START+1;i<ENUM_END;i++)
-    {
-	printf("%d\n",i);
         insertTable(strToEnum,symbol_map[i],i);
-    }
 }
 
 
